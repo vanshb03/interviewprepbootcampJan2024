267bfc6ff177b3046a56b3a2b957854471f8c199
Vansh Bataviya <bataviyavansh@gmail.com>
2024-01-03 11:19:02 -0500

    TwoSum Solved for Day 2

--- assignments/week1/Day 1 - Vansh/242_ValidAnagram.py
+++ assignments/week1/Day 1 - Vansh/242_ValidAnagram.py
@@ -1,9 +1,17 @@
+# Vansh B
+# Date: Jan 2, 2023
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #Converted both strings into lists
         list1 = list(s)
         list2 = list(t)
+        
+        #Sorted both lists:
         list1.sort()
         list2.sort()
+
+        #If both lists are same, then the ANagram is Valid else it is Not Valid.
         if list1 == list2:
             return True
         else:
